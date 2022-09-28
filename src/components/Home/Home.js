4814e53f0feee04c4a3ee7a2d41132cfbac1abc1
moddas311 <moddasir311@gmail.com>
2022-09-28 23:58:27 +0600

    completed fake api

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+           <div className="cart-container">
+                <h1>This is form cart container</h1>
+           </div>
+           <div className="cart-calculation">
+                <h4>This is from cart calculation</h4>
+           </div>
+        </div>
+    );
+};
+
+export default Home;
